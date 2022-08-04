307531ddc3b7c6098269ac5e50405c764cf7d258
Julien <denneulin.julien@gmail.com>
2022-08-04 07:59:04 +0200

    début construction dossier/fichier

---
+++ front/src/App.js
@@ -0,0 +1,16 @@
+
+import './styles/index.scss';
+
+function App() {
+  return (
+    <div className="App">
+     <p>hey salut</p>
+    <div className='conteneur'>
+      <h1>si je suis rouge le scss fonctionne !</h1>
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='conteneur'>
+          <h1>salut</h1>  
+        </div>
+    );
+};
+
+export default Header;
